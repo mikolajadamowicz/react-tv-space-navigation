6515aa20b3db40dadab2bc5b4c80a08134d087df
Mikolaj Adamowicz <mikolaj.adamowicz@homesoft.app>
2024-06-05 18:35:46 +0200

    small images

--- packages/example/src/modules/program/infra/programInfos.ts
+++ packages/example/src/modules/program/infra/programInfos.ts
@@ -11,18 +11,7 @@ import Rabbit10 from '../assets/rabbit10.png';
 import Rabbit11 from '../assets/rabbit11.png';
 import Rabbit12 from '../assets/rabbit12.png';
 import Rabbit13 from '../assets/rabbit13.png';
-import Rabbit14 from '../assets/rabbit14.png';
-import Rabbit15 from '../assets/rabbit15.png';
-import Rabbit16 from '../assets/rabbit16.png';
-import Rabbit17 from '../assets/rabbit17.png';
-import Rabbit18 from '../assets/rabbit18.png';
-import Rabbit19 from '../assets/rabbit19.png';
-import Rabbit20 from '../assets/rabbit20.png';
-import Rabbit21 from '../assets/rabbit21.png';
-import Rabbit22 from '../assets/rabbit22.png';
-import Rabbit23 from '../assets/rabbit23.png';
-import Rabbit24 from '../assets/rabbit24.png';
-import Rabbit25 from '../assets/rabbit25.png';
+
 import { ProgramInfo } from '../domain/programInfo';
 
 export const programInfos: ProgramInfo[] = [
@@ -103,78 +92,6 @@ export const programInfos: ProgramInfo[] = [
     title: 'Program 13',
     image: Rabbit13,
     description: 'Program 13 description',
-  },
-  {
-    id: '14',
-    title: 'Program 14',
-    image: Rabbit14,
-    description: 'Program 14 description',
-  },
-  {
-    id: '15',
-    title: 'Program 15',
-    image: Rabbit15,
-    description: 'Program 15 description',
-  },
-  {
-    id: '16',
-    title: 'Program 16',
-    image: Rabbit16,
-    description: 'Program 16 description',
-  },
-  {
-    id: '17',
-    title: 'Program 17',
-    image: Rabbit17,
-    description: 'Program 17 description',
-  },
-  {
-    id: '18',
-    title: 'Program 18',
-    image: Rabbit18,
-    description: 'Program 18 description',
-  },
-  {
-    id: '19',
-    title: 'Program 19',
-    image: Rabbit19,
-    description: 'Program 19 description',
-  },
-  {
-    id: '20',
-    title: 'Program 20',
-    image: Rabbit20,
-    description: 'Program 20 description',
-  },
-  {
-    id: '21',
-    title: 'Program 21',
-    image: Rabbit21,
-    description: 'Program 21 description',
-  },
-  {
-    id: '22',
-    title: 'Program 22',
-    image: Rabbit22,
-    description: 'Program 22 description',
-  },
-  {
-    id: '23',
-    title: 'Program 23',
-    image: Rabbit23,
-    description: 'Program 23 description',
-  },
-  {
-    id: '24',
-    title: 'Program 24',
-    image: Rabbit24,
-    description: 'Program 24 description',
-  },
-  {
-    id: '25',
-    title: 'Program 25',
-    image: Rabbit25,
-    description: 'Program 25 description',
   },
 ];
 
